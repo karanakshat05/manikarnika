7ee8c7bbcc883932649884f0dea08a144c53bc8c
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-08-13 23:32:23 +0530

    footer gradient

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0d220ddb29708d79897f01f0f3001c4021f510eb6b9a2c4eb9c9560e0ed1c43
-size 2743
+oid sha256:146d6ee2c7b6e7e61788f021117920f1fd861c62ba72e2e1e00da59add033dac
+size 2788
