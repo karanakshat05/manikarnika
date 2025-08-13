6a6cd5b2eda30a536dd6eda14a086ead7f99514e
Ashu Singh <142332675+karanakshat05@users.noreply.github.com>
2025-08-13 23:08:21 +0530

    about us effect

--- src/components/about_us.tsx
+++ src/components/about_us.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31bff7c854d5fa08559d4aa6cc6daf537a3ef95a2cd44c821dc5b983034341ea
-size 1780
+oid sha256:8e154808f1476d8909e4a144cfe6a74c4aafef2011ef778e6f227f5da450c87c
+size 1822
