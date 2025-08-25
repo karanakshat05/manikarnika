9cddd88c8ee190f7d17b6c04f6492185a8fb8cb0
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-08-25 21:06:19 +0530

    updated artist section

--- src/components/artists/ourartist.tsx
+++ src/components/artists/ourartist.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ba32f54a81fda0b2b987aa77ea187d73997023a6ceaeb33066318e5a469a91d
-size 3352
+oid sha256:b51070cc45107479cf4dc9683dea42d9c29ecdf457b60f1ace7de443fafb64ac
+size 4987
