2500386c44ddc9845888fcc5e85c541b8c6d1fda
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-08-26 00:05:44 +0530

    updated footer links

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65293779acfa8a7e365dc1ff7e5e990e577b2eeb5ab5c3367f09462a99507f57
-size 2908
+oid sha256:f6c10c11e23eeaed1af22061b1246c2fea2b0a8505facfdb34e8d4aced87380b
+size 3154
