9720348e63abacaf95683096152d41db8631f6ab
Ashu Singh <138131976+ashu1706@users.noreply.github.com>
2025-08-26 00:08:58 +0530

    Navbar update

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a5a38f0c2ae0d7cf09cbc6571842bfb51cd11b3717f79e558c7b5116b2dadee
+oid sha256:4707bb36526c0b023c259c63123c04840dfc407730b82a7b37db4d9c2f56a433
 size 1489
