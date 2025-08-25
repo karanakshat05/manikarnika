222e542f493b645654b2615954565bef07f5e3be
Ashu Singh <142332675+karanakshat05@users.noreply.github.com>
2025-08-25 21:29:11 +0530

    updated our work with motion frame

--- src/components/ourwork.tsx
+++ src/components/ourwork.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fec40fc1159fb11cbc145e4582778a1f799903cc920992989b66a51a64051d7
-size 2925
+oid sha256:b24bea01c0292677db88c62c74bb4aaa5530c9525865e30df052db2e5747ad1d
+size 2905
